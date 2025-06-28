23761e2e8bbbbaa76ffc7b991856088fed495ff5
SilviaFuregato <silvia.furegato@studio.unibo.it>
2025-06-28 17:23:28 +0200

    chore: Updated menu graphics

--- Assets/Scripts/PlayerAnimationController.cs
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -7,6 +7,8 @@ public class PlayerAnimationController : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private PlayerController player;
 
+    [SerializeField] private int lastFacing = 3;
+
     public void Start()
     {
         player.OnIdle += Idle;
@@ -17,8 +19,8 @@ public class PlayerAnimationController : MonoBehaviour
     public void Die()
     {
         animator.SetBool("IsDead", true);
-        animator.SetBool("IsDeadTrig", true);
         animator.SetBool("IsShooting", false);
+        animator.SetBool("IsMoving", false);
     }
 
     public void Idle(float x, float y)
@@ -27,12 +29,28 @@ public class PlayerAnimationController : MonoBehaviour
         animator.SetBool("IsShooting", false);
         animator.SetFloat("MoveX", x);
         animator.SetFloat("MoveY", y);
+        animator.SetInteger("Facing", lastFacing);
     }
 
     public void Walk(float x, float y)
     {
         animator.SetBool("IsMoving", true);
         animator.SetBool("IsShooting", false);
+        
+        if (y > 0 && x < y) {
+            lastFacing = 1;
+        }
+        else if (x > 0 && x > y) {
+            lastFacing = 2;
+        }
+        else if (y < 0 && x < y) {
+            lastFacing = 3;
+        }
+        else if (x < 0 && x > y)
+        {
+            lastFacing = 4;
+        }
+
         animator.SetFloat("MoveX", x);
         animator.SetFloat("MoveY", y);
     }
