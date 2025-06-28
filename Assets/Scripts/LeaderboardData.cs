7b033a0ef750ee775c90192e6b72aa5fd4f7e50b
Filippo Gurioli <filippo.gurioli@studio.unibo.it>
2025-06-28 15:22:19 +0200

    feat: create a bare minimal death ui

--- Assets/Scripts/LeaderboardData.cs
+++ Assets/Scripts/LeaderboardData.cs
@@ -11,5 +11,20 @@ public class LeaderboardData : ScriptableObject
     /// <summary>
     /// Leaderboard data
     /// </summary>
-    public List<(string name, int kills, DateTime date)> Data { get; } = new();
+    public List<DataItem> data = new();
+}
+
+[Serializable]
+public class DataItem
+{
+    public string name;
+    public int kills;
+    public DateTime date;
+    
+    public DataItem(string name, int kills)
+    {
+        this.name = name;
+        this.kills = kills;
+        date = DateTime.Now;
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,11 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float deathAnimationTime = 5f;
+    [SerializeField] private GameObject deathUI;
     
     private PlayerInputActions _inputActions;
     private Vector2 _moveInput;
@@ -29,8 +32,21 @@ public class PlayerController : MonoBehaviour
         _inputActions.Disable();
     }
 
-    private void FixedUpdate() // 👈 use FixedUpdate for Rigidbody2D
+    private void FixedUpdate()
     {
         _rb.velocity = _moveInput * moveSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Zombie")) return;
+        _inputActions.Disable();
+        StartCoroutine(StartDeathAnimation());
+    }
+
+    private IEnumerator StartDeathAnimation()
+    {
+        yield return new WaitForSeconds(deathAnimationTime);
+        deathUI.SetActive(true);
     }
 }
